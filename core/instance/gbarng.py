7a04daf20e730703b7177b178103fecdcf9bbab2
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2023-07-16 07:25:50 -0600

    initial seed detection

--- core/instance/gbarng.py
+++ core/instance/gbarng.py
@@ -23,6 +23,8 @@ class GBA:
         LEAFGREEN = ord("G")
         EMERALD = ord("E")
 
+    RSE = (GameVersion.RUBY, GameVersion.SAPPHIRE, GameVersion.EMERALD)
+
     class GameLanguage(IntEnum):
         """Gen 3 game langauge"""
         EUR = 0
@@ -75,20 +77,24 @@ class GBA:
                         self.party_addr = 0x03004290
                         self.wild_addr = 0x030044F0
                         self.id_addr = 0x02024C0E
+                        self.vframe_addr = 0x03001790
                     case self.GameLanguage.USA:
                         self.current_seed_addr = 0x03004818
                         self.party_addr = 0x03004360
                         self.wild_addr = 0x030045C0
                         self.id_addr = 0x02024EAE
+                        self.vframe_addr = 0x03001790
                     case self.GameLanguage.EUR:
                         self.current_seed_addr = 0x03004828
                         self.party_addr = 0x03004370
                         self.wild_addr = 0x030045D0
                         self.id_addr = 0x02024EAE
+                        self.vframe_addr = 0x03001790
             case self.GameVersion.FIRERED | self.GameVersion.LEAFGREEN:
                 self.initial_seed = 0
                 self.initial_seed_addr = 0x02020000
                 self.id_addr = None
+                self.vframe_addr = None
                 match self.game_language:
                     case self.GameLanguage.JPN:
                         if self.game_revision == 1:
@@ -119,26 +125,44 @@ class GBA:
                         self.party_addr = 0x02024190
                         self.wild_addr = 0x020243E8
                         self.sav2_addr = 0x03005AF0
+                        self.vframe_addr = 0x03002384
                     case self.GameLanguage.USA | self.GameLanguage.EUR:
                         self.current_seed_addr = 0x03005D80
                         self.party_addr = 0x020244EC
                         self.wild_addr = 0x02024744
                         self.sav2_addr = 0x03005D90
+                        self.vframe_addr = 0x030022E4
 
     def rng_info_window(self):
         """RNG seed info"""
 
-        with dpg.window(label="RNG Info", width=240, no_close=True, pos=[1, 100 + 25]):
+        with dpg.window(label="RNG Info", width=240, height=150, no_close=True, pos=[1, 100 + 25]):
+            if self.game_version in self.RSE:
+                detect_initial_seeding_check = dpg.add_checkbox(label = "Detect Initial Seeding")
             initial_seed_label = dpg.add_text("Initial Seed:")
             current_seed_label = dpg.add_text("Current Seed:")
             current_advance_label = dpg.add_text("Current Advance:")
+            if self.game_version in self.RSE:
+                painting_timer_label = dpg.add_text("Painting Timer:")
 
         def update():
+            current_seed = self.hook.read_uint(self.current_seed_addr, 4)
             if self.initial_seed_addr is not None:
                 self.initial_seed = self.hook.read_uint(self.initial_seed_addr, 2)
-            current_seed = self.hook.read_uint(self.current_seed_addr, 4)
+            if self.vframe_addr is not None:
+                vframe = self.hook.read_uint(self.vframe_addr, 4)
+                # only 2 bytes used for reseeding
+                painting_timer = vframe & 0xFFFF
+                dpg.set_value(painting_timer_label, f"Painting Timer: {painting_timer:02X}")
+                if dpg.get_value(detect_initial_seeding_check):
+                    # vframe == 0 only happens on game restart or 32 bit overflow (lol)
+                    if vframe == 0:
+                        self.initial_seed = self.hook.read_uint(self.current_seed_addr, 4)
+                    # painting_timer == current_seed on painting reseed or rare false positive
+                    if painting_timer == current_seed:
+                        self.initial_seed = self.hook.read_uint(self.current_seed_addr, 4)
             current_advance = lcrng_distance(self.initial_seed, current_seed)
-            dpg.set_value(initial_seed_label, f"Initial Seed: {self.initial_seed:04X}")
+            dpg.set_value(initial_seed_label, f"Initial Seed: {self.initial_seed:08X}")
             dpg.set_value(current_seed_label, f"Current Seed: {current_seed:08X}")
             dpg.set_value(current_advance_label, f"Current Advance: {current_advance}")
 
@@ -175,13 +199,13 @@ class GBA:
         return self.pokemon_info_window(
             self.wild_addr,
             "Wild",
-            [1, 201 + 25]
+            [1, 201 + 25 + 25 + 25]
         )
 
     def trainer_info_window(self):
         """Trainer info"""
 
-        with dpg.window(label="Trainer Info", width=240, no_close=True, pos=[1, 362 + 25]):
+        with dpg.window(label="Trainer Info", width=240, no_close=True, pos=[1, 362 + 25 + 25 + 25]):
             tid_sid_label = dpg.add_text("TID/SID:")
 
         def update():
